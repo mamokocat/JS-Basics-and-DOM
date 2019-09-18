35c132b54ef163ef74091228c18c372f4eef2e08
Artem <37788504+mamok0@users.noreply.github.com>
2019-09-18 16:06:47 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import Error404 from './Error404.js';
 import RouteHandler from './router.js';
 
 const routes = {
-  '': Home,
+  'JS-Basics-and-DOM': Home,
   search: Search,
   gif: Gif,
   error: Error404,
